016bf61fe3862cffb18e4e06a1544226a9eff0c1
thanoue <khoikhaguitar.vl@gmail.com>
2020-05-28 17:37:06 +0700

    edit login service

--- floridday-client/src/app/app.module.ts
+++ floridday-client/src/app/app.module.ts
@@ -71,16 +71,6 @@ export class AppModule {
 
   constructor(injector: Injector) {
     AppInjector.setInjector(injector);
-    firebase.initializeApp({
-      apiKey: 'AIzaSyDZGFKjLZH4h0SCRdmJVAP0QsRxo_9qYwA',
-      authDomain: 'lorid-e9c34.firebaseapp.com',
-      databaseURL: 'https://lorid-e9c34.firebaseio.com',
-      projectId: 'lorid-e9c34',
-      storageBucket: 'lorid-e9c34.appspot.com',
-      messagingSenderId: '907493762076',
-      appId: '1:907493762076:web:41a83454c12029c3c6abd9',
-      measurementId: 'G-DMM406R71M'
-    });
     registerLocaleData(es);
     registerLocaleData(vi);
   }
--- floridday-client/src/app/guards/login.guard.ts
+++ floridday-client/src/app/guards/login.guard.ts
@@ -3,15 +3,48 @@ import { CanActivate, Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { AuthService } from '../services/common/auth.service';
 import { Roles } from '../models/enums';
+import { GlobalService } from '../services/common/global.service';
 @Injectable({
     providedIn: 'root'
 })
 export class LoggedInGuard implements CanActivate {
-    constructor(private router: Router) {
+
+    constructor(private router: Router, private globalServie: GlobalService) {
     }
     canActivate() {
-        const user = firebase.auth().currentUser;
-        if (user) {
+
+        if (this.globalServie.firebaseIsInitialized) {
+            const user = firebase.auth().currentUser;
+            if (user) {
+                return true;
+            } else {
+                this.router.navigate(['login']);
+                return false;
+            }
+        } else {
+            this.router.navigate(['login']);
+            return false;
+        }
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+    constructor(private router: Router, private globalServie: GlobalService) {
+    }
+
+    canActivate() {
+
+        let loggedInGuard = new LoggedInGuard(this.router, this.globalServie);
+
+        if (!loggedInGuard.canActivate())
+            return false;
+
+        const role = (AuthService.getCurrentRole());
+        if (role == Roles.Admin) {
             return true;
         } else {
             this.router.navigate(['login']);
@@ -23,30 +56,25 @@ export class LoggedInGuard implements CanActivate {
 @Injectable({
     providedIn: 'root'
 })
-export class AccountGuard implements CanActivate {
-
-    constructor(private router: Router) {
-    }
-
-    canActivate() {
-        let loggedInGuard = new LoggedInGuard(this.router);
-
-        const role = (AuthService.getCurrentRole());
-        return loggedInGuard.canActivate() && (role == Roles.Admin || role == Roles.Account);
-    }
-}
-
-@Injectable({
-    providedIn: 'root'
-})
 export class FloristGuard implements CanActivate {
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private globalServie: GlobalService) {
     }
 
     canActivate() {
+
+        let loggedInGuard = new LoggedInGuard(this.router, this.globalServie);
+
+        if (!loggedInGuard.canActivate())
+            return false;
+
         const role = (AuthService.getCurrentRole());
-        return role == Roles.Florist;
+        if (role == Roles.Florist) {
+            return true;
+        } else {
+            this.router.navigate(['login']);
+            return false;
+        }
     }
 }
 
@@ -56,11 +84,22 @@ export class FloristGuard implements CanActivate {
 })
 export class ShipperGuard implements CanActivate {
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private globalServie: GlobalService) {
     }
 
     canActivate() {
+
+        let loggedInGuard = new LoggedInGuard(this.router, this.globalServie);
+
+        if (!loggedInGuard.canActivate())
+            return false;
+
         const role = (AuthService.getCurrentRole());
-        return role == Roles.Shipper;
+        if (role == Roles.Shipper) {
+            return true;
+        } else {
+            this.router.navigate(['login']);
+            return false;
+        }
     }
 }
